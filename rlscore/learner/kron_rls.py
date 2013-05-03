7de5464919de3f2100d66e0cb7ef7066fd94cc6a
aatapa <tapio.pahikkala@utu.fi>
2013-05-03 13:26:54 +0300

    Now also has a preliminary linear version and more cross-validation short-cuts.

--- rlscore/learner/kron_rls.py
+++ rlscore/learner/kron_rls.py
@@ -1,5 +1,5 @@
 
-
+import pyximport; pyximport.install()
 
 from numpy import *
 import numpy.linalg as la
@@ -8,6 +8,10 @@ from rlscore.learner.abstract_learner import AbstractLearner
 from rlscore import data_sources
 from rlscore import model
 from rlscore.utilities import array_tools
+from rlscore.utilities import decomposition
+
+from rlscore.utilities import sparse_kronecker_multiplication_tools
+
 
 class KronRLS(AbstractLearner):
 
@@ -51,6 +55,32 @@ class KronRLS(AbstractLearner):
         self.A = self.V * self.A * self.U.T
     
     
+    def solve_linear_hack(self, regparam):
+        self.regparam = regparam
+        X1 = mat(self.resource_pool['xmatrix1'])
+        X2 = mat(self.resource_pool['xmatrix2'])
+        
+        if not self.trained:
+            self.trained = True
+            L, V, rsvecs1 = decomposition.decomposeDataMatrix(X1.T)
+            self.L = L.T
+            self.V = V
+            self.rsvecs1 = mat(rsvecs1)
+            
+            S, U, rsvecs2 = decomposition.decomposeDataMatrix(X2.T)
+            self.S = S.T
+            self.U = U
+            self.rsvecs2 = mat(rsvecs2)
+            
+            self.VTYU = V.T * self.Y * U
+        
+        kronsvals = self.L * self.S.T
+        
+        newevals = divide(kronsvals, multiply(kronsvals, kronsvals) + regparam)
+        self.W = multiply(self.VTYU, newevals)
+        self.W = self.rsvecs1.T * self.W * self.rsvecs2
+    
+    
     def imputationLOO(self):
         P = self.K1.T * self.A * self.K2
         
@@ -70,7 +100,110 @@ class KronRLS(AbstractLearner):
         return loopred
     
     
+    def compute_ho(self, row_inds, col_inds):
+        
+        P_ho = self.K1.T[row_inds] * self.A * self.K2[:, col_inds]
+        
+        newevals = multiply(self.S * self.L.T, 1. / (self.S * self.L.T + self.regparam))
+        
+        rowcount = len(row_inds)
+        colcount = len(col_inds)
+        hosize = rowcount * colcount
+        
+        VV = mat(zeros((rowcount * rowcount, self.V.shape[1])))
+        UU = mat(zeros((colcount * colcount, self.U.shape[1])))
+        
+        def bar():
+            for i in range(len(row_inds)):
+                ith_row = self.V[row_inds[i]]
+                for h in range(len(row_inds)):
+                    VV[i * rowcount + h] = multiply(ith_row, self.V[row_inds[h]])
+            
+            for j in range(len(col_inds)):
+                jth_col = self.U[col_inds[j]]
+                for k in range(len(col_inds)):
+                    UU[j * colcount + k] = multiply(jth_col, self.U[col_inds[k]])
+        
+        def baz():
+            #print VV.shape, newevals.shape, UU.T.shape
+            B_in_wrong_order = VV * newevals.T * UU.T
+            
+            #B_in_right_order = mat(zeros((hosize, hosize)))
+            
+            #for i in range(len(row_inds)):
+            #    for j in range(len(col_inds)):
+            #        for h in range(len(row_inds)):
+            #            for k in range(len(col_inds)):
+            #                B_in_right_order[i * colcount + j, h * colcount + k] = B_in_wrong_order[i * rowcount + h, j * colcount + k]
+            
+            #print B_in_right_order
+            #print B_in_right_order.shape, B_in_wrong_order.shape, rowcount, colcount
+            B_in_right_order = mat(zeros((hosize, hosize)))
+            sparse_kronecker_multiplication_tools.cpy_reorder(B_in_right_order, B_in_wrong_order, rowcount, colcount)
+            #print B_in_right_order
+            #print
+            hopred = la.inv(mat(eye(hosize)) - B_in_right_order) * (P_ho.ravel().T - B_in_right_order * self.Y[ix_(row_inds, col_inds)].ravel().T)
+            return hopred
+        bar()
+        hopred = baz()
+        #print rowcount, colcount, hosize, hopred.shape
+        return hopred.reshape(rowcount, colcount)
+    
+    
     def nested_imputationLOO(self, outer_row_coord, outer_col_coord):
+        P = self.K1.T * self.A * self.K2
+        P_out = P[outer_row_coord, outer_col_coord]
+        Y_out = self.Y[outer_row_coord, outer_col_coord]
+        
+        newevals = multiply(self.S * self.L.T, 1. / (self.S * self.L.T + self.regparam))
+        Vsqr = multiply(self.V, self.V)
+        Usqr = multiply(self.U, self.U)
+        d = (Vsqr[outer_row_coord] * newevals.T * Usqr[outer_col_coord].T)[0, 0]
+        dY_out = d * Y_out
+        
+        Vox = multiply(self.V, self.V[outer_row_coord])
+        Uoy = multiply(self.U, self.U[outer_col_coord])
+        
+        cache = Vsqr * newevals.T * Usqr.T
+        crosscache = Vox * newevals.T * Uoy.T
+        
+        loopred = mat(zeros((self.V.shape[0], self.U.shape[0])))
+        #for i in range(self.V.shape[0]):
+        #    jinds = range(self.U.shape[0])
+        #    if i == outer_row_coord:
+        #        jinds.remove(outer_col_coord)
+#            for j in jinds:
+#                a = cache[i, j]
+#                bc = crosscache[i, j]
+#                invdetGshift = 1. / ((1. - a) * (1. - d) - bc * bc)
+#                invGshift_1 = invdetGshift * (1. - d)
+#                invGshift_2 = invdetGshift * bc
+#                Yij = self.Y[i, j]
+#                temp1 = P[i, j] - (a * Yij + bc * Y_out)
+#                temp2 = P_out - (bc * Yij + dY_out)
+#                loopred[i, j] = invGshift_1 * temp1 + invGshift_2 * temp2
+#            a = cache[i]
+#            bc = crosscache[i]
+#            invdetGshift = divide(1., ((1. - a) * (1. - d) - multiply(bc, bc)))
+#            invGshift_1 = invdetGshift * (1. - d)
+#            invGshift_2 = multiply(invdetGshift, bc)
+#            Yi = self.Y[i]
+#            temp1 = P[i] - (multiply(a, Yi) + bc * Y_out)
+#            temp2 = P_out - (multiply(bc, Yi) + dY_out)
+#            loopred[i] = multiply(invGshift_1, temp1) + multiply(invGshift_2, temp2)
+        a = cache
+        bc = crosscache
+        invdetGshift = divide(1., ((1. - a) * (1. - d) - multiply(bc, bc)))
+        invGshift_1 = invdetGshift * (1. - d)
+        invGshift_2 = multiply(invdetGshift, bc)
+        Y = self.Y
+        temp1 = P - (multiply(a, Y) + bc * Y_out)
+        temp2 = P_out - (multiply(bc, Y) + dY_out)
+        loopred = multiply(invGshift_1, temp1) + multiply(invGshift_2, temp2)
+        return loopred
+    
+    
+    def nested_imputationLOO_BU(self, outer_row_coord, outer_col_coord):
         P = self.K1.T * self.A * self.K2
         P_out = P[outer_row_coord, outer_col_coord]
         Y_out = self.Y[outer_row_coord, outer_col_coord]
@@ -251,6 +384,20 @@ class PairwiseModel(object):
     
     def predictWithKernelMatrices(self, K1pred, K2pred):
         P = K1pred.T * self.A * K2pred
+        return P
+
+
+class PairwiseModelLinearHack(object):
+    
+    def __init__(self, W):
+        """Initializes the linear model
+        @param W: primal coefficient matrix
+        @type W: numpy matrix"""
+        self.W = W
+    
+    
+    def predictWithDataMatrices(self, X1pred, X2pred):
+        P = X1pred * self.W * X2pred.T
         return P
 
 
