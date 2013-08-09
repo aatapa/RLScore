b7503dbfbf52d23cadb581ef0ddcce4e213700c4
ajairo <ajairo@utu.fi>
2013-08-09 16:53:37 +0300

    Numpy includes added to setup.

--- cython_setup.py
+++ cython_setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
 
 ext_modules = [Extension(
     name="rlscore.utilities.swapped",
     sources=["ext_src/swapped.pyx", "ext_src/c_swapped.c"],
-    language="c"),
-    Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc.pyx"]),
-    Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls.pyx"]),
-    Extension("rlscore.utilities.sparse_kronecker_multiplication_tools",["rlscore/utilities/sparse_kronecker_multiplication_tools.pyx"])]
+    language="c", include_dirs=[np.get_include()]),
+    Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc.pyx"], include_dirs=[np.get_include()]),
+    Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls.pyx"], include_dirs=[np.get_include()]),
+    Extension("rlscore.utilities.sparse_kronecker_multiplication_tools",["rlscore/utilities/sparse_kronecker_multiplication_tools.pyx"], include_dirs=[np.get_include()])]
 
 setup(
     name = 'cmodules',
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from distutils.command import build_ext
+import numpy as np
 
 ext_modules = [Extension(
     name="rlscore.utilities.swapped",
     sources=["ext_src/swapped.c", "ext_src/c_swapped.c"],
-    language="c"),
-    Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc.c"]),
-    Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls.c"]),
-    Extension("rlscore.utilities.sparse_kronecker_multiplication_tools",["rlscore/utilities/sparse_kronecker_multiplication_tools.c"])]
+    language="c", include_dirs=[np.get_include()]),
+    Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc.c"], include_dirs=[np.get_include()]),
+    Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls.c"], include_dirs=[np.get_include()]),
+    Extension("rlscore.utilities.sparse_kronecker_multiplication_tools",["rlscore/utilities/sparse_kronecker_multiplication_tools.c"], include_dirs=[np.get_include()])
+]
 
 setup(
     name = 'RLScore',
     ext_modules = ext_modules,
     )
+
 
