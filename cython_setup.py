03e0fbb772caafff4a3db18eba17afbd42fdf3db
Tapio Pahikkala <aatapa@utu.fi>
2015-10-09 15:14:28 +0300

    Now compiles the pairwise CV extension for RLS.

--- cython_setup.py
+++ cython_setup.py
@@ -3,10 +3,12 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-ext_modules = [Extension("rlscore.utilities.swapped",["rlscore/utilities/swapped.pyx"], include_dirs=[np.get_include()] ),
+ext_modules = [Extension("rlscore.utilities.swapped",["rlscore/utilities/swapped.pyx"], include_dirs=[np.get_include()]),
+    Extension("rlscore.learner.cython_pairwise_cv_for_rls",["rlscore/learner/cython_pairwise_cv_for_rls.pyx"], include_dirs=[np.get_include()]),
     Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc.pyx"], include_dirs=[np.get_include()]),
     Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls.pyx"], include_dirs=[np.get_include()]),
-    Extension("rlscore.utilities.sparse_kronecker_multiplication_tools",["rlscore/utilities/sparse_kronecker_multiplication_tools.pyx"], include_dirs=[np.get_include()])]
+    Extension("rlscore.utilities.sparse_kronecker_multiplication_tools",["rlscore/utilities/sparse_kronecker_multiplication_tools.pyx"], include_dirs=[np.get_include()])
+    ]
 
 setup(
     name = 'cmodules',
