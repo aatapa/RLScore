964b3f30617e46170f1cd6f7f047a6a5e4a17eda
ajairo <ajairo@utu.fi>
2015-10-21 09:43:15 +0300

    New cross-validation routines

---
+++ rlscore/utilities/cross_validation.py
@@ -0,0 +1,38 @@
+import random
+import numpy as np
+
+def random_folds(ssize, k, seed = None):
+    #returns indices from 0...ssize-1 split to k random folds
+    if seed != None:
+        myrandom = random.Random(seed)
+    else:
+        myrandom = random
+    folds = []
+    indices = set(range(ssize))
+    foldsize = ssize / k
+    leftover = ssize % k
+    for i in range(k):
+        sample_size = foldsize
+        if leftover > 0:
+            sample_size += 1
+            leftover -= 1
+        fold = myrandom.sample(indices, sample_size)
+        indices = indices.difference(fold)
+        folds.append(fold)
+    return folds
+
+def grid_search(crossvalidator, grid):
+    #used internally for fast regularization parameter selection
+    performances = []
+    predictions = []
+    for regparam in grid:
+        perf, P = crossvalidator.cv(regparam)
+        performances.append(perf)
+        predictions.append(P)
+    if crossvalidator.measure.iserror:
+        bestparam = grid[np.argmin(performances)]
+    else:
+        bestparam = grid[np.argmax(performances)]
+    learner = crossvalidator.rls
+    learner.solve(bestparam)
+    return performances, predictions
