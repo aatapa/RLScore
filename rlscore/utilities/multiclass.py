856512cb56fc8c7faec5b2d252ac2c3b46bc08c6
ajairo <ajairo@utu.fi>
2016-02-04 17:03:52 +0200

    Added tools for multi.class classification

---
+++ rlscore/utilities/multiclass.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def to_one_vs_all(Y):
+    #maps vector of class labels from range {1,..,classcount} to matrix,
+    #of size ssize x classes
+    Y = np.array(Y)
+    Y_ova = -1. * np.ones((Y.shape[0], np.max(Y)))
+    for i in range(len(Y)):
+        Y_ova[i, Y[i]-1] = 1
+    return Y_ova
+
+def from_one_vs_all(Y):
+    #maps one-vs-all encoding or predictions to classes ranging 1...classcount
+    return np.argmax(Y, axis=1) + 1
