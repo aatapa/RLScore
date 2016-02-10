24ebf78debc6921f4f8b2b5d0cf7e12d833c5751
ajairo <ajairo@utu.fi>
2016-02-10 17:14:04 +0200

    Setup script can now properly install rlscore if needed

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
 import numpy as np
 import sys
 
-sys.argv[1:] = ['build_ext', '--inplace']
+USE_CYTHON = False
+ext = '.pyx' if USE_CYTHON else '.c'
+
+#sys.argv[1:] = ['build_ext', '--inplace']
 
 ext_modules = [
-    Extension("rlscore.utilities.swapped",["rlscore/utilities/swapped.c"], include_dirs=[np.get_include()] ),
-    Extension("rlscore.learner.cython_pairwise_cv_for_rls",["rlscore/learner/cython_pairwise_cv_for_rls.c"], include_dirs=[np.get_include()]),
-    Extension("rlscore.learner.cython_pairwise_cv_for_global_rankrls",["rlscore/learner/cython_pairwise_cv_for_global_rankrls.c"], include_dirs=[np.get_include()]),
-    Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc.c"], include_dirs=[np.get_include()]),
-    Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls.c"], include_dirs=[np.get_include()]),
-    Extension("rlscore.utilities._sampled_kronecker_products",["rlscore/utilities/_sampled_kronecker_products.c"], include_dirs=[np.get_include()])
+    Extension("rlscore.utilities.swapped",["rlscore/utilities/swapped"+ext]),
+    Extension("rlscore.learner.cython_pairwise_cv_for_rls",["rlscore/learner/cython_pairwise_cv_for_rls"+ext]),
+    Extension("rlscore.learner.cython_pairwise_cv_for_global_rankrls",["rlscore/learner/cython_pairwise_cv_for_global_rankrls"+ext]),
+    Extension("rlscore.learner.cython_mmc",["rlscore/learner/cython_mmc"+ext]),
+    Extension("rlscore.learner.cython_greedy_rls",["rlscore/learner/cython_greedy_rls"+ext]),
+    Extension("rlscore.utilities._sampled_kronecker_products",["rlscore/utilities/_sampled_kronecker_products"+ext])
 ]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    ext_modules = cythonize(ext_modules)
 
 setup(
     name = 'RLScore',
+    url = "https://github.com/aatapa/RLScore",
+    version = "0.93",
+    license = "MIT",
+    #setup_requires=["numpy"],
+    #install_requires=["numpy", "scipy"],
+    include_dirs = [np.get_include()],
     ext_modules = ext_modules,
+    packages = find_packages(),
     )
 
 
