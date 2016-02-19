2252a2316e1bcc6bf67310ebcf1eec7bdbd663e8
ajairo <ajairo@utu.fi>
2016-02-19 14:05:25 +0200

    GlobalRankRLS test

--- rlscore/test/test_learner/test_global_rankrls.py
+++ rlscore/test/test_learner/test_global_rankrls.py
@@ -1,12 +1,52 @@
-import sys
 import unittest
-
+from numpy.testing import assert_allclose
 import numpy as np
 import numpy.linalg as la
 
 from rlscore.learner import GlobalRankRLS
 
 class Test(unittest.TestCase):
+    
+    def setUp(self):
+        np.random.seed(100)
+        m= 30
+        self.Xtrain1 = np.random.rand(m, 20)
+        self.Xtrain2 = np.random.rand(m, 40)
+        self.Ytrain1 = np.random.randn(m)
+        self.Ytrain2 = np.random.randn(m, 5)
+        self.bvectors = [0,3,5,22]
+        
+    def test_linear(self):
+        #Test that learning with linear kernel works correctly both
+        #with low and high-dimensional data
+        for X in [self.Xtrain1, self.Xtrain2]:
+            for Y in [self.Ytrain1, self.Ytrain2]:
+                #Basic case
+                m = X.shape[0]
+                L = m * np.eye(m) -  np.ones((m,m))
+                primal_rls = GlobalRankRLS(X, Y, regparam=1.0, bias=0.)
+                W = primal_rls.predictor.W
+                d = X.shape[1]
+                W2 = np.linalg.solve(np.dot(X.T, np.dot(L, X)) + np.eye(d), np.dot(X.T, np.dot(L, Y)))
+                assert_allclose(W, W2)
+                #For RankRLS, bias should have no effect
+                primal_rls = GlobalRankRLS(X, Y, regparam=1.0, bias=5.)
+                W2 = primal_rls.predictor.W
+                assert_allclose(W, W2)
+                #Fast regularization
+                primal_rls.solve(10)
+                W = primal_rls.predictor.W
+                W2 = np.linalg.solve(np.dot(X.T, np.dot(L, X)) + 10 * np.eye(d), np.dot(X.T, np.dot(L, Y)))
+                assert_allclose(W, W2)
+                #reduced set approximation
+                primal_rls = GlobalRankRLS(X, Y, basis_vectors = X[self.bvectors], regparam=1.0)
+                W = primal_rls.predictor.W
+                K = np.dot(X, X.T)
+                Kr = K[:, self.bvectors]
+                Krr = K[np.ix_(self.bvectors, self.bvectors)]
+                A = np.linalg.solve(np.dot(Kr.T, np.dot(L, Kr))+ 1.0 * Krr, np.dot(Kr.T, np.dot(L, Y)))
+                W2 = np.dot(X[self.bvectors].T, A)
+                assert_allclose(W, W2)
     
     def testAllPairsRankRLS(self):
         
