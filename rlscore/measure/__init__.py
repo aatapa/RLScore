0fbb37348e7f6c42fe41f5fecd75d3eb7cf973e2
ajairo <ajairo@utu.fi>
2016-03-15 13:58:36 +0200

    Removed old dependency on disagreement error.

--- rlscore/measure/__init__.py
+++ rlscore/measure/__init__.py
@@ -1,7 +1,6 @@
 
 from accuracy_measure import accuracy
 from auc_measure import auc
-from disagreement_measure import disagreement
 from fscore_measure import fscore
 from multi_accuracy_measure import ova_accuracy
 from sq_mprank_measure import sqmprank
