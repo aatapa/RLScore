11827da7226d3f6cfed6367eb1efce1179eef92d
ajairo <ajairo@utu.fi>
2017-04-12 10:35:04 +0300

    cleaned old commented code

--- rlscore/test/test_learner/test_kronsvm.py
+++ rlscore/test/test_learner/test_kronsvm.py
@@ -32,16 +32,6 @@ def primal_svm_objective(v, X1, X2, Y, rowind, colind, lamb):
     return 0.5*(np.dot(z,z)+lamb*np.dot(v,v))
 
 def load_data(primal=True, fold_index=0):
-    #fname =  "legacy_tests/data/FOLDS-nr-q4"
-    #f = open(fname)
-    #dfolds, tfolds = cPickle.load(f)
-    #dfold = dfolds[fold_index / 3]
-    #tfold = tfolds[fold_index % 3]
-    #Y = np.loadtxt('legacy_tests/data/nr_admat_dgc.txt')
-    #Y = np.where(Y>=0.5, 1., -1.)
-
-    #X1 = np.loadtxt('legacy_tests/data/nr_simmat_dc.txt')
-    #X2 = np.loadtxt('legacy_tests/data/nr_simmat_dg.txt')
     X1 = np.random.rand(20, 300)
     X2 = np.random.rand(10, 200)
     dfold = [1,2,4,5]
